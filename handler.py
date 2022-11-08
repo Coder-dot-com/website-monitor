55cf27a4281f64f3c78527aa0843de0a20aff247
usman17.s@hotmail.com <usman17.s@hotmail.com>
2022-11-08 21:31:20 +0000

    Create script

--- handler.py
+++ handler.py
@@ -8,29 +8,8 @@ from sib_api_v3_sdk.models.send_smtp_email_to import SendSmtpEmailTo
 from sib_api_v3_sdk.rest import ApiException
 from pprint import pprint
 from decouple import config
+from datetime import datetime
 
-
-# Configure API key authorization: api-key
-configuration = sib_api_v3_sdk.Configuration()
-configuration.api_key['api-key'] = config('SENDBLUE_API_SECRET')
-# Uncomment below to setup prefix (e.g. Bearer) for API key, if needed
-# configuration.api_key_prefix['api-key'] = 'Bearer'
-# Configure API key authorization: partner-key
-
-
-# create an instance of the API class
-api_instance = sib_api_v3_sdk.AccountApi(sib_api_v3_sdk.ApiClient(configuration))
-# create an instance of the API class
-api_instance = sib_api_v3_sdk.TransactionalEmailsApi(sib_api_v3_sdk.ApiClient(configuration))
-send_smtp_email = sib_api_v3_sdk.SendSmtpEmail(sender=SendSmtpEmailSender(email="team@allmediterranean.com"), to=[SendSmtpEmailTo(email="team@allmediterranean.com")], html_content="Website is down", text_content="Website is down",  subject="Website is down") # SendSmtpEmail | Values to send a transactional email
-#    def __init__(self, sender=None, to=None, bcc=None, cc=None, html_content=None, text_content=None, subject=None, reply_to=None, attachment=None, headers=None, template_id=None, params=None, message_versions=None, tags=None, scheduled_at=None, batch_id=None):  # noqa: E501
-
-try:
-    # Send a transactional email
-    api_response = api_instance.send_transac_email(send_smtp_email)
-    pprint(api_response)
-except ApiException as e:
-    print("Exception when calling TransactionalEmailsApi->send_transac_email: %s\n" % e)
 
 #Monitor the website
 def monitor_website(event, context):
@@ -41,7 +20,26 @@ def monitor_website(event, context):
        if status != 200:
             #Call email function
             print("The website is down")
+            timestamp = datetime.now()
 
+            email_content = f"Website is down. Status: {status} Time: {timestamp}"
+            configuration = sib_api_v3_sdk.Configuration()
+            configuration.api_key['api-key'] = config('SENDBLUE_API_SECRET')
+
+            # create an instance of the API class
+            api_instance = sib_api_v3_sdk.TransactionalEmailsApi(sib_api_v3_sdk.ApiClient(configuration))
+            send_smtp_email = sib_api_v3_sdk.SendSmtpEmail(sender=SendSmtpEmailSender(email="team@allmediterranean.com"), 
+            to=[SendSmtpEmailTo(email="team@allmediterranean.com"), SendSmtpEmailTo(email="usman17.s@hotmail.com")],
+            html_content=email_content,
+            text_content=email_content,
+            subject="Website is down") # SendSmtpEmail | Values to send a transactional email
+
+            try:
+                # Send a transactional email
+                api_response = api_instance.send_transac_email(send_smtp_email)
+                pprint(api_response)
+            except ApiException as e:
+                print("Exception when calling TransactionalEmailsApi->send_transac_email: %s\n" % e)
 
 
        else:
@@ -50,4 +48,3 @@ def monitor_website(event, context):
 
 
   
-monitor_website(1, 1)
